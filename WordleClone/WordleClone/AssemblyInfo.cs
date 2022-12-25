ae79582cb4c7539faec392480f548cdb88bfdaf2
Scott Peterts <Scottpeters2281@gmail.com>
2022-12-24 21:45:01 -0800

    Add project files.

---
+++ WordleClone/WordleClone/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Courgette-Regular.ttf", Alias = "Courgette")]
+[assembly: ExportFont("FontAwesome-Regular.otf", Alias = "fa-regular")]
+[assembly: ExportFont("FontAwesome-Solid.otf", Alias = "fa-solid")]
